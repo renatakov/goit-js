96718a344d3c2f0d68e32ed005d716f673d162d1
renatakov <renatakov1010@gmail.com>
2020-03-27 11:23:31 +0200

    work is done

--- homework-2/task-1.js
+++ homework-2/task-1.js
@@ -19,7 +19,7 @@ fruits[5] =  "персики";
 const test1 = [4, 12, 6, 16, 13, 8, 13, 4, 15, 11, 14, 10, 15, 12];
 const score1 = 12;
 
-    for (let i = 0; i < test1.length i += 1) {
+    for (let i = 0; i < test1.length; i += 1) {
         if (test1[i] < score1) {
        continue;
         }
@@ -31,7 +31,7 @@ const score1 = 12;
 const test2 = [17, 13, 4, 15, 10, 12, 16, 21, 13, 8, 14, 18, 15, 20];
 const score2 = 15;
 
-for (let i = 0; i < test2.length i += 1) {
+for (let i = 0; i < test2.length; i += 1) {
     if (test2[i] < score2) {
    continue;
     }
@@ -43,7 +43,7 @@ for (let i = 0; i < test2.length i += 1) {
 const test3 = [20, 12, 16, 22, 13, 8, 13, 4, 15, 10, 14, 18, 15, 22];
 const score3 = 16;
 
-for (let i = 0; i < test1.length) {
+for (let i = 0; i < test1.length; i += 1) {
     if (test1[i] < score3) {
    continue;
     }
